f476ccfd8a2b2b33aa08bc26a67c95757b00f9a0
HitkoDev <development@hitko.si>
2021-01-28 19:38:48 +0100

    ds

--- src/awe_dataset.py
+++ src/awe_dataset.py
@@ -9,19 +9,17 @@ class AWEDataset(object):
         images = {}
         classes = []
         for d in os.listdir(path):
-            for lr in ['L', 'R']:
-                c = '{}_{}'.format(d, lr)
-                classes.append(c)
-                dir = os.path.join(path, d, lr)
-                for f in glob.glob(dir + '/*.png'):
-                    if c not in images:
-                        images[c] = []
-                    images[c].append({
-                        "subject": d,
-                        "lr": lr,
-                        "src": f,
-                        "mask": f[:-4] + '.npy',
-                        "class": c
-                    })
+            c = d
+            classes.append(c)
+            dir = os.path.join(path, d)
+            for f in glob.glob(dir + '/*.png'):
+                if c not in images:
+                    images[c] = []
+                images[c].append({
+                    "subject": d,
+                    "src": f,
+                    "mask": f[:-4] + '.npy',
+                    "class": c
+                })
         self.images = [images[k] for k in images]
         self.classes = classes
