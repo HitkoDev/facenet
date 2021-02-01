2c9df57958641f88945e9de9299e71f7db8ff439
HitkoDev <development@hitko.si>
2021-02-02 00:35:48 +0100

    nt

--- src/train_tripletloss.py
+++ src/train_tripletloss.py
@@ -273,9 +273,9 @@ def train(args, sess, dataset, epoch, image_paths_placeholder, labels_placeholde
         nrof_triplets = 0
         while nrof_triplets * 3 < args.batch_size * 2:
             triplets, nrof_random_negs, nrof_triplets = select_triplets(emb_array, num_per_class, image_paths, args.people_per_batch, alpha)
-            alpha = alpha * 1.1
-        nrof_triplets = int(np.floor(nrof_triplets * 3 / args.batch_size) * args.batch_size / 3)
-        triplets = triplets[0:nrof_triplets]
+            alpha = alpha * 1.01
+        # nrof_triplets = int(np.floor(nrof_triplets * 3 / args.batch_size) * args.batch_size / 3)
+        # triplets = triplets[0:nrof_triplets]
         selection_time = time.time() - start_time
         print('(nrof_random_negs, nrof_triplets) = (%d, %d): time=%.3f seconds' %
               (nrof_random_negs, nrof_triplets, selection_time))
